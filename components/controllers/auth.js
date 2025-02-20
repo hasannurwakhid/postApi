28004d0a0d5e6ebefa9c697b42e106eaf3fc40bf
Hasan Nur Wakhid <hasannurwakhid@gmail.com>
2025-02-20 09:02:14 +0700

    update validation on auth

--- components/controllers/auth.js
+++ components/controllers/auth.js
@@ -1,27 +1,47 @@
 const { register, login } = require("../services/auth");
+const Joi = require("joi");
 
 exports.register = async (req, res, next) => {
   try {
-    const { username, password, name } = req?.body;
+    const schema = Joi.object({
+      username: Joi.string()
+        .pattern(/^\S+$/)
+        .alphanum()
+        .min(4)
+        .max(50)
+        .required()
+        .empty()
+        .messages({
+          "string.pattern.base": "Username tidak boleh mengandung spasi!",
+          "string.alphanum": "Username hanya boleh mengandung huruf dan angka!",
+          "string.min": "Username minimal 4 karakter!",
+          "string.max": "Username maksimal 50 karakter!",
+          "string.empty": "Username harus diisi!",
+          "any.required": "Username diperlukan!",
+        }),
+      password: Joi.string().min(6).max(100).required().empty().messages({
+        "string.min": "Password minimal 6 karakter!",
+        "string.max": "Password maksimal 100 karakter!",
+        "string.empty": "Password harus diisi!",
+        "any.required": "Password diperlukan!",
+      }),
+      name: Joi.string().min(2).max(100).required().empty().messages({
+        "string.min": "Nama minimal 2 karakter!",
+        "string.max": "Nama maksimal 100 karakter!",
+        "string.empty": "Name harus diisi!",
+        "any.required": "Name diperlukan!",
+      }),
+    });
 
-    if (username == "" || !username) {
+    const { error } = schema.validate(req.body);
+    if (error) {
       return next({
-        message: "Username harus diisi!",
+        message: error.details[0].message,
         statusCode: 400,
       });
     }
-    if (password == "" || !password) {
-      return next({
-        message: "Password harus diisi!",
-        statusCode: 400,
-      });
-    }
-    if (name == "" || !name) {
-      return next({
-        message: "Name harus diisi!",
-        statusCode: 400,
-      });
-    }
+
+    const { username, password, name } = req?.body;
 
     const data = await register({
       username,
@@ -39,20 +59,26 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
-    const { username, password } = req?.body;
+    const schema = Joi.object({
+      username: Joi.string().required().empty().messages({
+        "string.empty": "Username harus diisi!",
+        "any.required": "Username diperlukan!",
+      }),
+      password: Joi.string().required().empty().messages({
+        "string.empty": "Password harus diisi!",
+        "any.required": "Password diperlukan!",
+      }),
+    });
 
-    if (username == "" || !username) {
+    const { error } = schema.validate(req.body);
+    if (error) {
       return next({
-        message: "Username harus diisi!",
+        message: error.details[0].message,
         statusCode: 400,
       });
     }
-    if (password == "" || !password) {
-      return next({
-        message: "Password harus diisi!",
-        statusCode: 400,
-      });
-    }
+
+    const { username, password } = req?.body;
 
     const data = await login({
       username,
